6e711c598c4b7c3cf31d1f2c62e96861578fc0fb
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-05-22 23:02:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,9 @@ const App = () => {
     
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <ol>
+      {cityList.map((city,index)=>(<li key={`location${index+1}`}>{city.name}</li>))}
+      </ol>
     </div>
   )
 }
